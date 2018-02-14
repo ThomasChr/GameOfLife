f94b39d3c1fd10d8373d78d9ca15076a934b2b26
Bernd Stellwag <burned@zerties.org>
2018-02-14 20:19:16 +0100

    some more refactoring

--- game.go
+++ game.go
@@ -31,35 +31,36 @@ func main() {
 }
 
 func getRandomValue() rune {
-    if rand.Intn(10) == 0 {
-        return 'X'
-    }
+	if rand.Intn(10) == 0 {
+		return 'X'
+	}
 
-    return ' '
+	return ' '
 }
 
 func printWorld(myworld [][]rune) {
 	// clear screen
 	fmt.Print("\033[H\033[2J")
 
-	for y := 0; y < rows; y++ {
+	for y := range myworld {
 		fmt.Println(string(myworld[y]))
 	}
 }
 
 func evolveWorld(myworld [][]rune) [][]rune {
 	var newworld = make([][]rune, rows)
-	for y := 0; y < rows; y++ {
+	for y, row := range myworld {
 		newworld[y] = make([]rune, cols)
-		for x := 0; x < cols; x++ {
+		for x, current := range row {
 			var neighborCount = getNumOfNeighbors(myworld, x, y)
-			var current = myworld[y][x]
-			var newVal = current
 
+			var newVal rune
 			if current == ' ' && neighborCount == 3 {
 				newVal = 'X'
 			} else if current == 'X' && (neighborCount < 2 || neighborCount > 3) {
 				newVal = ' '
+			} else {
+				newVal = current
 			}
 
 			newworld[y][x] = newVal
